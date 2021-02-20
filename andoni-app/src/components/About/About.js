daf870e1dc8dea2840c03608fdee2a25fd656755
Javier González <doniagv@gmail.com>
2021-02-20 15:10:26 -0600

    Hero about finished

--- andoni-app/src/components/About/About.js
+++ andoni-app/src/components/About/About.js
@@ -1,21 +1,25 @@
 import React from "react";
 import "./About.css";
+import personal from "../../assets/images/personal.jpg";
 
 const About = () => {
   return (
     <div id="about">
       <div className="inner-about">
-        <h2>About Me</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          velit metus, pharetra non eros eget, feugiat posuere purus. Sed felis
-          massa, bibendum ac dapibus eu, dignissim eu ex. Praesent odio sapien,
-          feugiat eget enim sit amet, bibendum tincidunt lorem. Mauris ipsum ex,
-          faucibus sit amet laoreet aliquet, ornare sit amet nulla. In nibh
-          purus, ullamcorper quis augue vitae, sagittis egestas sapien. Nam
-          mattis pulvinar est eu auctor. Cras nec eros ac massa pretium sodales
-          a non turpis.
-        </p>
+        <div>
+          <h2>About Me</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            velit metus, pharetra non eros eget, feugiat posuere purus. Sed
+            felis massa, bibendum ac dapibus eu, dignissim eu ex. Praesent odio
+            sapien, feugiat eget enim sit amet, bibendum tincidunt lorem. Mauris
+            ipsum ex, faucibus sit amet laoreet aliquet, ornare sit amet nulla.
+            In nibh purus, ullamcorper quis augue vitae, sagittis egestas
+            sapien. Nam mattis pulvinar est eu auctor. Cras nec eros ac massa
+            pretium sodales a non turpis.
+          </p>
+        </div>
+        <img src={personal} alt="PersonalPhoto"></img>
       </div>
     </div>
   );
--- andoni-app/src/components/Hero/Hero.js
+++ andoni-app/src/components/Hero/Hero.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Hero.css";
+import LogoP from "../../assets/images/croped-logo.png";
 
 const Hero = () => {
-  return <div id="home" className="Hero"></div>;
+  return (
+    <div id="home" className="Hero">
+      <img src={LogoP} alt="logo" />
+      <button className="Button-Do">Download CV</button>
+    </div>
+  );
 };
 
 export default Hero;
